ec3a199704bc2e376145009da893e740c22385c9
katiefrost95 <katie_frost95@hotmail.co.uk>
2018-02-05 17:05:49 +0000

    user and computer return input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,37 +4,50 @@ import javax.swing.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    alert();
-    }
+    int playerScore = 0;
+    int conputerScore = 0;
 
-    // Creating alert
-    private static void alert() {
+
+    public static void main(String[] args) {
+        String compPick;
+
         // Creating text field for alert box
-        JTextField rpsOption = new JTextField();
+        JTextField userInput = new JTextField();
         final JComponent[] inputs = new JComponent[] {
                 new JLabel("Hi there! Lets play rock, paper, scissors. Which do you pick? "),
-                rpsOption,
+                userInput,
         };
         // saving user input and giving back a response in console.
         int result = JOptionPane.showConfirmDialog(null, inputs, "Rock, Paper, Scissors", JOptionPane.PLAIN_MESSAGE);
         // If pressed okay save to console
         if (result == JOptionPane.OK_OPTION) {
             System.out.println("You entered " +
-                    rpsOption.getText() + ", ");
-            // if comp pick and user pick the same we draw
-            // if comp pick and user pick diff one win/one lose
+                    userInput.getText() + ", ");
+            //Computer input
+            double computerInput = Math.floor(Math.random()*3);
+            if (computerInput == 0) {
+                compPick = "rock";
+            } else if (computerInput == 1) {
+                compPick = "scissors";
+            } else if (computerInput == 2) {
+                compPick = "paper";
+            } else {
+                compPick = "invalid";
+            }
+            System.out.println("Computer entered " + compPick);
+                // comparing user and computer input
+            if (compPick == userInput.getText()) {
+                System.out.println("This is a draw");
+            } else {
+                System.out.println("This was not a draw");
+            }
+
         } else {
             System.out.println("User canceled / closed the dialog, result = " + result);
         }
+
     }
 
-    private static void computerChoice() {
-        double computerInput = Math.floor(Math.random()*3);
-        String comp_pick;
-        if (computerInput.equals("0")) {
-            comp_pick.equals("rock");
-        }
-    }
+
 }
 
